6caef1ee6a5d311cd567be0b38cde8cda6712d01
Matthew Harding <41506964+Acacia-Developer@users.noreply.github.com>
2018-07-21 11:04:57 +0100

    Added scripts for two new videos

---
+++ [Unity C#] Implementing a 3D Skybox/SkyboxCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxCamera : MonoBehaviour
+{
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private float skyboxScale;
+
+
+    private void Update()
+    {
+        transform.rotation = playerCamera.rotation;
+        transform.localPosition = playerCamera.position / skyboxScale;
+    }
+
+}

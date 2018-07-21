97befc26874cde501914f6ccefa7b5e6edfe9ef0
Matthew Harding <41506964+Acacia-Developer@users.noreply.github.com>
2018-07-21 11:00:57 +0100

    Added scripts for two new videos

---
+++ [Unity C#] Exchanging Data Between Scripts (Fields, Properties and Static Keyword)/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Admin : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Player.runSpeed = 8.03f;
+    }
+
+    void Start()
+    {
+
+    }
+
+}
---
+++ [Unity C#] Exchanging Data Between Scripts (Fields, Properties and Static Keyword)/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private int health;
+
+    public int Health
+    {
+        get { return health; }
+        set
+        {
+            if (value < 0 || value > 100)
+                Debug.LogError("Health is less than 0 or larger than 100!");
+            else
+            {
+                health = value;
+                
+                if (value == 0)
+                {
+                    isDead = true;
+                    //function call to play death animation
+                }
+                    
+            }
+                  
+                
+        }
+    }
+
+    public bool isDead;
+    public static float walkSpeed;
+    public static float runSpeed;
+
+    void Start()
+    {
+        Debug.Log(runSpeed);
+    }
+
+}
